e9e8fb7f81be18d58c96804c077fc3d42add7e31
Philippe Brossier <brossierp@gmail.com>
2017-09-18 11:59:46 +0100

    CTPA-1556 notifyGW still starts

--- src/main/java/uk/gov/ons/ctp/response/notify/config/NotifyConfiguration.java
+++ src/main/java/uk/gov/ons/ctp/response/notify/config/NotifyConfiguration.java
@@ -4,7 +4,7 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import uk.gov.service.notify.NotificationClient;
+import uk.gov.service.notify.NotificationClient;
 
 /**
  * Configuration specific to GOV.UK Notify
@@ -17,13 +17,13 @@ public class NotifyConfiguration {
 
   private String censusUacSmsTemplateId;
   private String onsSurveysRasEmailReminderTemplateId;
-//
-//  /**
-//   * To set up the GOV.UK Notify NotificationClient
-//   * @return the notificationClient
-//   */
-//  @Bean
-//  public NotificationClient notificationClient() {
-//    return new NotificationClient(apiKey);
-//  }
+
+  /**
+   * To set up the GOV.UK Notify NotificationClient
+   * @return the notificationClient
+   */
+  @Bean
+  public NotificationClient notificationClient() {
+    return new NotificationClient(apiKey);
+  }
 }
