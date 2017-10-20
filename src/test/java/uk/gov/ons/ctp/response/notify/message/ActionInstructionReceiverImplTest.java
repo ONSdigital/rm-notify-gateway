49c04fe5a671429687fb6ad399e2261bad9c52ad
sdiaz1000 <sdiaz1000@gmail.com>
2017-10-20 11:54:52 +0100

    CTPA-1677 use new api release to loosen email validation

--- src/test/java/uk/gov/ons/ctp/response/notify/message/ActionInstructionReceiverImplTest.java
+++ src/test/java/uk/gov/ons/ctp/response/notify/message/ActionInstructionReceiverImplTest.java
@@ -74,40 +74,6 @@ public class ActionInstructionReceiverImplTest {
     verify(actionFeedbackPublisher, times(1)).sendFeedback(eq(mockedActionFeedback));
   }
 
-  @Test
-  public void testProcessBRESInstructionInvalidEmailAddress() throws NotificationClientException {
-    ActionFeedback mockedActionFeedback = new ActionFeedback(MOCKED_ACTIONID,
-        NOTIFY_EMAIL_SENT.length() <= SITUATION_MAX_LENGTH ?
-            NOTIFY_EMAIL_SENT : NOTIFY_EMAIL_SENT.substring(0, SITUATION_MAX_LENGTH), Outcome.REQUEST_COMPLETED);
-    when(notifyService.process(any(ActionRequest.class))).thenReturn(mockedActionFeedback);
-
-    actionInstructionReceiver.processInstruction(
-        ObjectBuilder.buildActionInstruction(
-            "9a5f2be5-f944-41f9-982c-3517cfcfef3c", "Joe,Blogg,0774,@gmail.com", true));
-
-    verify(notifyService, times(0)).process(any(ActionRequest.class));
-
-    ArgumentCaptor<ActionFeedback> argumentCaptor = ArgumentCaptor.forClass(ActionFeedback.class);
-    verify(actionFeedbackPublisher, times(2)).sendFeedback(argumentCaptor.capture());
-    List<ActionFeedback> actionFeedbackList = argumentCaptor.getAllValues();
-    assertEquals(2, actionFeedbackList.size());
-    int countAccepted = 0; int countDeclined = 0; int countCompleted = 0;
-    for (ActionFeedback anActionFeedback :  actionFeedbackList) {
-      if (anActionFeedback.getOutcome().equals(Outcome.REQUEST_ACCEPTED)) {
-        countAccepted += 1;
-      }
-      if (anActionFeedback.getOutcome().equals(Outcome.REQUEST_DECLINED)) {
-        countDeclined += 1;
-      }
-      if (anActionFeedback.getOutcome().equals(Outcome.REQUEST_COMPLETED)) {
-        countCompleted += 1;
-      }
-    }
-    assertEquals(1, countAccepted);
-    assertEquals(1, countDeclined);
-    assertEquals(0, countCompleted);
-    verify(actionFeedbackPublisher, times(0)).sendFeedback(eq(mockedActionFeedback));
-  }
 
   @Test
   public void testProcessBRESInstructionEmailAddressWithSpacesAtFrontAndBack() throws NotificationClientException {
